bd24d171b1c9e2b146e98861cfcdebe08bceaefb
RuebenHodges <79278437+RuebenHodges@users.noreply.github.com>
2023-12-10 20:03:02 +0000

    attempting to run program

--- external/taskList.php
+++ external/taskList.php
@@ -4,12 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
+    <title>TaskList/title>
     <link rel="stylesheet" href="tasklist.css">
 </head>
 
 <body>
-    <?php include("addtask.php")?>
     <header>
         <h1>Task List | add Task | </h1>
         <nav class = "nav">
